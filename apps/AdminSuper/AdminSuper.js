8c5ce428994927d917c0fc6074229090cd81cc2c
DD斩首 <11475223+hutao222@user.noreply.gitee.com>
2023-01-10 10:14:46 +0000

    update apps/AdminSuper/AdminSuper.js.
    Signed-off-by: DD斩首 <11475223+hutao222@user.noreply.gitee.com>

--- apps/AdminSuper/AdminSuper.js
+++ apps/AdminSuper/AdminSuper.js
@@ -951,6 +951,9 @@ export async function synchronization(e) {
         if (!isNotNull(player.幸运)) {
             player.幸运 = 0;
         }
+        if (!isNotNull(player.皮肤)) {
+            player.皮肤 = 0;
+        }
         if (!isNotNull(player.islucky)) {
             player.islucky = 0;
         }
