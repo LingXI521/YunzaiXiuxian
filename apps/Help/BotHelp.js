dea25eac22e10bf09c968ef29635cd66f267605f
再氪两单嘛 <11993349+two-more-kryptons@user.noreply.gitee.com>
2023-01-14 18:34:36 +0000

    update apps/Help/BotHelp.js.
    Signed-off-by: 再氪两单嘛 <11993349+two-more-kryptons@user.noreply.gitee.com>

--- apps/Help/BotHelp.js
+++ apps/Help/BotHelp.js
@@ -1,12 +1,12 @@
-
-import plugin from '../../../../lib/plugins/plugin.js'
-import puppeteer from "../../../../lib/puppeteer/puppeteer.js";
-import Help from "../../model/help.js";
-import md5 from "md5";
+import plugin from '../../../../lib/plugins/plugin.js';
+import puppeteer from '../../../../lib/puppeteer/puppeteer.js';
+import Help from '../../model/help.js';
+import Help1 from '../../model/xinchunhelp.js';
+import md5 from 'md5';
 
 let helpData = {
-    md5: "",
-    img: "",
+  md5: '',
+  img: '',
 };
 
 /**
@@ -14,89 +14,98 @@ let helpData = {
  */
 
 export class BotHelp extends plugin {
-    constructor() {
-        super({
-            /** 功能名称 */
-            name: 'BotHelp',
-            /** 功能描述 */
-            dsc: '修仙帮助',
-            event: 'message',
-            /** 优先级，数字越小等级越高 */
-            priority: 400,
-            rule: [
-                {
-                    reg: '^#修仙帮助$',
-                    fnc: 'Xiuxianhelp'
-                },
-                {
-                    reg: "^#修仙管理$",
-                    fnc: "adminsuper",
-                },
-                {
-                    reg: "^#宗门管理$",
-                    fnc: "AssociationAdmin",
-                },
-                {
-                    reg: "^#修仙扩展$",
-                    fnc: "Xiuxianhelpcopy",
-                }
-            ]
-        })
+  constructor() {
+    super({
+      /** 功能名称 */
+      name: 'BotHelp',
+      /** 功能描述 */
+      dsc: '修仙帮助',
+      event: 'message',
+      /** 优先级，数字越小等级越高 */
+      priority: 400,
+      rule: [
+        {
+          reg: '^#修仙帮助$',
+          fnc: 'Xiuxianhelp',
+        },
+        {
+          reg: '^#修仙管理$',
+          fnc: 'adminsuper',
+        },
+        {
+          reg: '^#宗门管理$',
+          fnc: 'AssociationAdmin',
+        },
+        {
+          reg: '^#修仙扩展$',
+          fnc: 'Xiuxianhelpcopy',
+        },
+        {
+          reg: '^#新春帮助$',
+          fnc: 'xinchunhelp',
+        },
+      ],
+    });
+  }
+  async xinchunhelp(e) {
+    if (!e.isGroup) {
+      return;
     }
+    let data = await Help1.xinchunhelp(e);
+    if (!data) return;
+    let img = await this.cache(data);
+    await e.reply(img);
+  }
 
-    async Xiuxianhelpcopy(e){
-        if (!e.isGroup) {
-           return;
-       }
-       let data = await Help.gethelpcopy(e);
-       if (!data) return;
-       let img = await this.cache(data);
-       await e.reply(img);
+  async Xiuxianhelpcopy(e) {
+    if (!e.isGroup) {
+      return;
     }
+    let data = await Help.gethelpcopy(e);
+    if (!data) return;
+    let img = await this.cache(data);
+    await e.reply(img);
+  }
 
-
-
-    /**
-     * rule - 修仙帮助
-     * @returns
-     */
-    async Xiuxianhelp(e) {
-         if (!e.isGroup) {
-            return;
-        }
-        let data = await Help.get(e);
-        if (!data) return;
-        let img = await this.cache(data);
-        await e.reply(img);
+  /**
+   * rule - 修仙帮助
+   * @returns
+   */
+  async Xiuxianhelp(e) {
+    if (!e.isGroup) {
+      return;
     }
+    let data = await Help.get(e);
+    if (!data) return;
+    let img = await this.cache(data);
+    await e.reply(img);
+  }
 
-
-    async adminsuper(e) {
-         if (!e.isGroup) {
-            return;
-        }
-        let data = await Help.setup(e);
-        if (!data) return;
-        let img = await this.cache(data);
-        await e.reply(img);
+  async adminsuper(e) {
+    if (!e.isGroup) {
+      return;
     }
+    let data = await Help.setup(e);
+    if (!data) return;
+    let img = await this.cache(data);
+    await e.reply(img);
+  }
 
-    async AssociationAdmin(e) {
-         if (!e.isGroup) {
-            return;
-        }
-        let data = await Help.Association(e);
-        if (!data) return;
-        let img = await this.cache(data);
-        await e.reply(img);
+  async AssociationAdmin(e) {
+    if (!e.isGroup) {
+      return;
     }
+    let data = await Help.Association(e);
+    if (!data) return;
+    let img = await this.cache(data);
+    await e.reply(img);
+  }
 
-    async cache(data) {
-        let tmp = md5(JSON.stringify(data));
-        if (helpData.md5 == tmp) return helpData.img;
-        helpData.img = await puppeteer.screenshot("help", data);
-        helpData.md5 = tmp;
-        return helpData.img;
-    }
-
+  async cache(data) {
+    let tmp = md5(JSON.stringify(data));
+    if (helpData.md5 == tmp) return helpData.img;
+    helpData.img = await puppeteer.screenshot('help', data);
+    helpData.md5 = tmp;
+    return helpData.img;
+  }
 }
