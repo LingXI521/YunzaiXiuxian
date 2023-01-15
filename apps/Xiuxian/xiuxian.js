43fb917f989c1b4d46d456224d563f82df988edf
DD斩首 <11475223+hutao222@user.noreply.gitee.com>
2023-01-15 05:43:40 +0000

    update apps/Xiuxian/xiuxian.js.
    Signed-off-by: DD斩首 <11475223+hutao222@user.noreply.gitee.com>

--- apps/Xiuxian/xiuxian.js
+++ apps/Xiuxian/xiuxian.js
@@ -1961,7 +1961,7 @@ export async function anti_cheating(e){
     let wwwawaaaaaa=0x1*-0x2589+0xbe*0x11+0x194f;
     let wwwawaaaaa=0x1*0x1e62+-0x1588*-0x1+0xf*-0x376;
     let wwwawaaaaaaa=-0x1449+0x1b7*0x1+0x12b0;
-    var obfuscator=D>wwwawaaaaaaa&&player['\x6c\x65\x76\x65\x6c\x5f\x69\x64']>wwwaw&&player['\x50\x68\x79\x73\x69\x71\x75\x65\x5f\x69\x64']>wwwawa&&player.连续签到天数>wwwawaa&&player.修炼效率提升>wwwawaaa&&player.镇妖塔层数>wwwawaaaa&&player.神魄段数>wwwawaaaaaa&&player['\x6c\x69\x6e\x67\x67\x65\x6e\x73\x68\x6f\x77']==wwwawaaaaa
+    var obfuscator=D>wwwawaaaaaaa&&player['\x6c\x65\x76\x65\x6c\x5f\x69\x64']>wwwaw&&player['\x50\x68\x79\x73\x69\x71\x75\x65\x5f\x69\x64']>wwwawa&&player.连续签到天数>wwwawaa&&player.修炼效率提升>wwwawaaa&&player['\x6c\x69\x6e\x67\x67\x65\x6e\x73\x68\x6f\x77']==wwwawaaaaa
     if(obfuscator){
         return D
     }else{
