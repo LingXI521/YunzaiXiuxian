31c09888303895911abd45f0ce58db27b1d7d4de
闹钟不闹 <12165227+mrzh_13612471008_163@user.noreply.gitee.com>
2023-01-18 14:14:18 +0000

    update apps/Occupation/Occupation.js.
    Signed-off-by: 闹钟不闹 <12165227+mrzh_13612471008_163@user.noreply.gitee.com>

--- apps/Occupation/Occupation.js
+++ apps/Occupation/Occupation.js
@@ -87,6 +87,14 @@ export class Occupation extends plugin {
                 {
                     reg: '^#刺杀目标.*$',
                     fnc: 'cisha_sb'
+                },
+                {
+                    reg: '(^#狩猎$)|(^#狩猎(.*)(分|分钟)$)',
+                    fnc: 'shoulie'
+                },
+                {
+                    reg: '^#结束狩猎$',
+                    fnc: 'shoulie_back'
                 }
             ]
         });
