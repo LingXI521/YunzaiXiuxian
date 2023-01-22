2359b9e633618e7ba89fa5eb0090b8b6c32d2a7f
闹钟不闹 <12165227+mrzh_13612471008_163@user.noreply.gitee.com>
2023-01-22 03:15:20 +0000

    update apps/User/UserHome.js.
    Signed-off-by: 闹钟不闹 <12165227+mrzh_13612471008_163@user.noreply.gitee.com>

--- apps/User/UserHome.js
+++ apps/User/UserHome.js
@@ -3227,7 +3227,7 @@ if (thing_name == "羊毛") {
                 let math=Math.random();
                 let number1 = await exist_najie_thing(usr_qq, "羊毛", "食材")
                 if (isNotNull(number1)  && number1 >  quantity-1 ) {
-                    await Add_najie_thing(usr_qq, "羊毛", "材料",  -quantity);
+                    await Add_najie_thing(usr_qq, "羊毛", "食材",  -quantity);
                     await Add_najie_thing(usr_qq, "线", "材料",  quantity);
                     e.reply(["合成成功，获得线" +quantity + "个"])
                     return
