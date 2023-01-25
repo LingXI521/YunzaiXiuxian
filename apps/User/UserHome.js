41d5711913cb76d7719e3979a1e31b8cb70cbfe8
闹钟不闹 <12165227+mrzh_13612471008_163@user.noreply.gitee.com>
2023-01-25 05:22:44 +0000

    update apps/User/UserHome.js.
    Signed-off-by: 闹钟不闹 <12165227+mrzh_13612471008_163@user.noreply.gitee.com>

--- apps/User/UserHome.js
+++ apps/User/UserHome.js
@@ -2697,9 +2697,8 @@ if (thing_name == "羊毛") {
                             await Write_player(usr_qq, player);
                             await redis.set("xiuxian:player:" + usr_qq + "xunbaocd", now_Time);
                             if (isNotNull(mugao) && mugao >  quantity - 1){
-                                if(mugao>15){
-                                    mugao=15;
-                                    e.reply("一个地图最多使用15个镐子")
+                                if(mugao>10){
+                                    mugao=10;
                                 }
                                 await Add_najie_thing(usr_qq, "圆石", "材料", 18*mugao*n);
                                 await Add_najie_thing(usr_qq, "煤炭", "材料", 18*mugao*n);
@@ -2707,9 +2706,8 @@ if (thing_name == "羊毛") {
                                 await Add_najie_thing(usr_qq, "铁镐", "道具", -1*mugao);
                             }else{mugao=0;}
                             if(isNotNull(shigao) && shigao >  quantity - 1){
-                                if(shigao>15){
-                                    shigao=15;
-                                    e.reply("一个地图最多使用15个镐子")
+                                if(shigao>10){
+                                    shigao=10;
                                 }
                                 await Add_najie_thing(usr_qq, "圆石", "材料", 9*shigao*n);
                                 await Add_najie_thing(usr_qq, "煤炭", "材料", 9*shigao*n);
