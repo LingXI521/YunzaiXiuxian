7b431478389b02fed54bf6ff81a378bd9f7eeb7b
wuhuan666 <1589978621@qq.com>
2023-01-26 22:57:50 +0800

    尝试修复比试错误

--- apps/Tiandibang/Tiandibang.js
+++ apps/Tiandibang/Tiandibang.js
@@ -380,6 +380,7 @@ export class Tiandibang extends plugin {
                     blood=1.3;
                 }
                 B_player = {
+                    id:tiandibang[k].qq,
                     名号: tiandibang[k].名号,
                     攻击: tiandibang[k].攻击,
                     防御: tiandibang[k].防御,
@@ -391,6 +392,7 @@ export class Tiandibang extends plugin {
                 }
             }
             let A_player = {
+                id:tiandibang[x].qq,
                 名号: tiandibang[x].名号,
                 攻击: parseInt((tiandibang[x].攻击))*atk,
                 防御: parseInt((tiandibang[x].防御)*def),
