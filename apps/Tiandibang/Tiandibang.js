ae1a923896f41f92fa31f83cc14a0f4953097d63
wuhuan666 <1589978621@qq.com>
2023-01-26 23:02:48 +0800

    修复比试错误

--- apps/Tiandibang/Tiandibang.js
+++ apps/Tiandibang/Tiandibang.js
@@ -408,6 +408,7 @@ export class Tiandibang extends plugin {
                 def=(0.8+0.4*Math.random());
                 blood=(0.8+0.4*Math.random());
                 B_player = {
+                    id:tiandibang[x].qq,
                     名号: "灵修兽",
                     攻击: parseInt((tiandibang[x].攻击))*atk,
                     防御: parseInt((tiandibang[x].防御)*def),
