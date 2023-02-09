d89e98d4d30917267c2561604b45896a0b868095
闹钟不闹 <12165227+mrzh_13612471008_163@user.noreply.gitee.com>
2023-02-09 02:16:59 +0000

    update apps/User/UserHome.js.
    Signed-off-by: 闹钟不闹 <12165227+mrzh_13612471008_163@user.noreply.gitee.com>

--- apps/User/UserHome.js
+++ apps/User/UserHome.js
@@ -1151,6 +1151,7 @@ export class UserHome extends plugin {
 
                             if (action[i].biguan > 0) { action[i].biguan += this_danyao.机缘 * quantity }
                             if (action[i].lianti > 0) { action[i].lianti += this_danyao.机缘 * quantity }
+                            if (action[i].ped    > 0) { action[i].ped    += this_danyao.机缘 * quantity }
                             if (action[i].beiyong2 > 0) { action[i].beiyong2 += this_danyao.机缘 * quantity }
                             e.reply(`丹韵入体,身体内蕴含的仙丹药效增加了${this_danyao.机缘 * quantity}次`)
                             await redis.set("xiuxian:player:" + 10 + ":biguang", JSON.stringify(action))
