d98f1a583368869030ef2f5b281bd59658fa2c3c
闹钟不闹 <12165227+mrzh_13612471008_163@user.noreply.gitee.com>
2023-02-09 02:18:58 +0000

    update apps/SecretPlace/SecretPlaceplusTask.js.
    Signed-off-by: 闹钟不闹 <12165227+mrzh_13612471008_163@user.noreply.gitee.com>

--- apps/SecretPlace/SecretPlaceplusTask.js
+++ apps/SecretPlace/SecretPlaceplusTask.js
@@ -402,7 +402,7 @@ export class SecretPlaceplusTask extends plugin {
                 }else if(random2 > 0.9&&random2<=0.95) {
                   caoyao += '"魔蕴花"';
                   await Add_najie_thing(player_id, '魔蕴花', '草药', 1);
-                }else if (random2 > 0.88&&random2<=0.9) {
+                }else if (random2 > 0.88&&random2<0.89) {
                   caoyao += '"太玄仙草"';
                   await Add_najie_thing(player_id, '太玄仙草', '草药', 1);
                 }else if (random2 > 0.83&&random2<=0.88) {
@@ -421,7 +421,7 @@ export class SecretPlaceplusTask extends plugin {
                 if (
                   random2 > 0.95&&random2<=1 ||
                  random2 > 0.9&&random2<=0.95 ||
-                 random2 > 0.88&&random2<=0.9 ||
+                 random2 > 0.88&&random2<0.89 ||
                   random2 > 0.83&&random2<=0.88 ||
                  random2>0&&random2<=0.005||
                   random2 > 0.80&&random2<=0.83||
