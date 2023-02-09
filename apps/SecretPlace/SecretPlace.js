fa89db2061570f1af98013984015d0e3db259622
闹钟不闹 <12165227+mrzh_13612471008_163@user.noreply.gitee.com>
2023-02-09 17:14:16 +0000

    update apps/SecretPlace/SecretPlace.js.
    Signed-off-by: 闹钟不闹 <12165227+mrzh_13612471008_163@user.noreply.gitee.com>

--- apps/SecretPlace/SecretPlace.js
+++ apps/SecretPlace/SecretPlace.js
@@ -272,6 +272,12 @@ export class SecretPlace extends plugin {
                 return
         }
     }
+     if(didian=="诸神黄昏·旧神界"){
+         if (now_level_id < 41) {
+            e.reply("没有达到仙人之前还是不要去了")
+            return;
+        }
+    }
         if (player.灵石 < weizhi.Price) {
             e.reply("没有灵石寸步难行,攒到" + weizhi.Price + "灵石才够哦~");
             return true;
