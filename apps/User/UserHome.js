b70104b88f2e8a2a31edc25cf9f79f8e4bf666ac
闹钟不闹 <12165227+mrzh_13612471008_163@user.noreply.gitee.com>
2023-02-10 04:26:27 +0000

    update apps/User/UserHome.js.
    Signed-off-by: 闹钟不闹 <12165227+mrzh_13612471008_163@user.noreply.gitee.com>

--- apps/User/UserHome.js
+++ apps/User/UserHome.js
@@ -3046,7 +3046,7 @@ export class UserHome extends plugin {
                   }else if(math>0.7&&math<=0.8){
                     await Add_najie_thing(usr_qq, "树苗", "食材", 50*n);
                     e.reply(`${last_msg}${fyd_msg}你在深渊捡到了深渊石${9*muchan*n+9*shichan*n+18*zuanshichan*n}个,
-                    下界合金矿${9*zuanshichan*n}个,额外捡到深渊石${100*n}个,获得了修为${xiuwei}血气${xueqi}`)
+                    下界合金矿${9*zuanshichan*n}个,额外捡到树苗${50*n}个,获得了修为${xiuwei}血气${xueqi}`)
                     return;
                   }else if(math>0.1&&math<=0.2){
                      await Add_najie_thing(usr_qq, "岩浆", "材料", 15*n);
